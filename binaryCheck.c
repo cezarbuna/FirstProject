35b5332eae7ba17373f6b34bb5ba96505e3e9793
cezarbuna <93285504+cezarbuna@users.noreply.github.com>
2021-12-01 23:49:05 +0200

    added source code file

---
+++ binaryCheck.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int isBinary(int n)
+{
+    while(n)
+    {
+        if((n%10)!=0&&(n%10)!=1)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    printf("%d",isBinary(111));
+    return 0;
+}
